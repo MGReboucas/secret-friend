1e7857a83f0273b5b47208cd4334f4edc1ac7ad0
Matheus <reboucas444@gmail.com>
2025-07-08 11:13:08 -0300

    validation to not add if the friend input value is empty and the number of friends less than four

--- js/app.js
+++ js/app.js
@@ -7,24 +7,33 @@ function adicionar() {
      let friendsList = document.getElementById('lista-amigos');
      friends.push(friend.value);
      //writing values ​​to the p tag
-     if (friendsList.textContent == '') {
-          friendsList.textContent = friend.value;
+     if (friend.value != '') {
+          if (friendsList.textContent == '') {
+               friendsList.textContent = friend.value;
+               friend.value = "";
+          } else {
+               friendsList.textContent = friendsList.textContent + ', ' + friend.value;
+               friend.value = "";
+          }
      } else {
-          friendsList.textContent = friendsList.textContent + ', ' + friend.value;
+          alert('Ops, Você não digitou nada, digite um nome valido');
      }
-     friend.value = "";
 }
 
 function sortear() {
-     embaralhar(friends);
      let sorteio = document.getElementById('lista-sorteio');
 
-     for (let i = 0; i < friends.length; i++) {
+     if (friends.length >= 4) {
+          embaralhar(friends);
+          for (let i = 0; i < friends.length; i++) {
           if (i == friends.length - 1) {
                sorteio.innerHTML = sorteio.innerHTML + friends[i] + '--->' + friends[0] + '<br>';
           } else {
                sorteio.innerHTML = sorteio.innerHTML + friends[i] + '--->' + friends[i + 1] + '<br>';
           }
+     }
+     } else {
+          alert('Por favor, adicione mais nomes para a brincadeira rolar');
      }
 }
 
@@ -38,5 +47,8 @@ function embaralhar(friendsList) {
 }
 
 function reiniciar() {
-
+     friends = [];
+     document.getElementById('nome-amigo').value = '';
+     document.getElementById('lista-amigos').innerHTML = '';
+     document.getElementById('lista-sorteio').innerHTML = '';
 }
